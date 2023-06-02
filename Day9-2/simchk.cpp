f548365c79ad7ad53f812865d6c62f3fdcc08ba3
kkh <dhadgfl@gmail.com>
2023-06-02 11:58:41 +0900

    length check commit

--- Day9-2/simchk.cpp
+++ Day9-2/simchk.cpp
@@ -1,4 +1,48 @@
+#include <string>
+
+using namespace std;
+
 class SimilarityChecker {
 public:
+	int getLengthSimilarityScore(const string& input1, const string& input2) {
+		int input1Length = input1.length();
+		int input2Length = input2.length();
 
+		if (checkSameLength(input1Length, input2Length)) {
+			return 60;
+		}
+
+		if (checkDoubleLength(input1Length, input2Length)) {
+			return 0;
+		}
+
+		int longStrLength = getLongLength(input1Length, input2Length);
+		int shortStrLength = getShortLength(input1Length, input2Length);
+		int gap = longStrLength - shortStrLength;
+		return calculateLengthSimilarityScore(shortStrLength, gap);
+	}
+
+private:
+	bool checkSameLength(int input1Length, int input2Length) {
+		return input1Length == input2Length;
+	}
+
+	bool checkDoubleLength(int input1Length, int input2Length) {
+		return input1Length * 2 <= input2Length
+			|| input2Length * 2 <= input1Length;
+	}
+
+	int getLongLength(int input1Length, int input2Length) {
+		if (input1Length > input2Length) return input1Length;
+		return input2Length;
+	}
+
+	int getShortLength(int input1Length, int input2Length) {
+		if (input1Length > input2Length) return input2Length;
+		return input1Length;
+	}
+
+	int calculateLengthSimilarityScore(int shortStrLength, int gap) {
+		return 60 - (gap * 60 / shortStrLength);
+	}
 };
--- Sample-Test1/simchk_test.cpp
+++ Sample-Test1/simchk_test.cpp
@@ -1,6 +1,30 @@
 #include "pch.h"
 #include "../Day9-2/simchk.cpp"
 
-TEST(SimilarityChecker, SC) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityChecker, Length_Same) {
+	string input1 = "ASD";
+	string input2 = "DSA";
+	int score = SimilarityChecker().getLengthSimilarityScore(input1, input2);
+	EXPECT_EQ(60, score);
+}
+
+TEST(SimilarityChecker, Length_Larger_Equal_Double) {
+	string input1 = "A";
+	string input2 = "BB";
+	int score = SimilarityChecker().getLengthSimilarityScore(input1, input2);
+	EXPECT_EQ(0, score);
+}
+
+TEST(SimilarityChecker, Length_Less_Double_1) {
+	string input1 = "AAABB";
+	string input2 = "BAA";
+	int score = SimilarityChecker().getLengthSimilarityScore(input1, input2);
+	EXPECT_EQ(20, score);
+}
+
+TEST(SimilarityChecker, Length_Less_Double_2) {
+	string input1 = "AA";
+	string input2 = "AAA";
+	int score = SimilarityChecker().getLengthSimilarityScore(input1, input2);
+	EXPECT_EQ(30, score);
 }
